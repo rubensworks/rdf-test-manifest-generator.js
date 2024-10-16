ccf6dbf76f7100a21f647f433bb56cbc5458e388
Ruben Taelman <ruben.taelman@ugent.be>
2024-10-16 14:16:00 +0200

    Make generation more abstract wrt positive/negative tests

--- generate.js
+++ generate.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 
 const filesNames = process.argv.slice(2)
-    .map(path => path.slice(path.lastIndexOf('/') + 1));
+    .map(path => path.slice(path.lastIndexOf('/') + 1))
+    .filter(path => !path.includes('manifest'));
+
+const firstFile = process.argv.slice(2)[0];
+const type = firstFile.includes('negative') ? 'Negative' : 'Positive';
+const path = firstFile.substring(0, firstFile.indexOf('/'));
 
 const labels = filesNames
     .map(name => name.slice(0, name.lastIndexOf('.')))
@@ -9,28 +14,26 @@ const labels = filesNames
     .join('');
 
 const entries = filesNames
-    .map(name => `:${name.slice(0, name.lastIndexOf('.'))} rdf:type   mf:NegativeSyntaxTest11 ;
+    .map(name => `:${name.slice(0, name.lastIndexOf('.'))} rdf:type   mf:${type}${name.includes('update') ? 'Update' : ''}SyntaxTest11 ;
    dawgt:approval dawgt:Proposed ;
    mf:name    "${name}" ;
-   mf:action  <${name}> .\n\n`)
-    .join('');
+   mf:action  <${name}> .`)
+    .join('\n\n');
 
 console.log(
-`@prefix :       <http://www.w3.org/2009/sparql/docs/tests/data-sparql11/syntax-query/manifest#> .
+`@prefix :       <https://w3c.github.io/rdf-tests/sparql/sparql12/${path}/manifest#> .
 @prefix rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
 @prefix rdfs:   <http://www.w3.org/2000/01/rdf-schema#> .
 @prefix mf:     <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#> .
-@prefix mfx:    <http://jena.hpl.hp.com/2005/05/test-manifest-extra#> .
 @prefix qt:     <http://www.w3.org/2001/sw/DataAccess/tests/test-query#> .
 @prefix dawgt:  <http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#> .
 
 <>  rdf:type mf:Manifest ;
-    rdfs:label "Negative Syntax Triple Terms" ;
-    rdfs:comment "Negative syntax tests for triple terms in SPARQL 1.2" ;
+    rdfs:label "${type} Syntax Triple Terms" ;
+    rdfs:comment "${type} syntax tests for triple terms in SPARQL 1.2" ;
     mf:entries
     (
 ${labels}
 ) .
 
-${entries}
-`);
+${entries}`);
